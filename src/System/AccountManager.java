a15aa84dbb7bfc89cb526311c9cf49eeca325153
Ishanya Kukade <ishanyak12@gmail.com>
2024-08-01 11:43:47 +0530

    Add transfer_money() in AccountManager

--- src/System/AccountManager.java
+++ src/System/AccountManager.java
@@ -22,4 +22,15 @@ public class AccountManager {
 		System.out.println("Enter Security Pin: ");
 		String security_pin = scanner.nextLine();
 	}
+
+	public void transfer_money(long sender_amount_number) {
+		scanner.nextLine();
+		System.out.println("Enter Reciever Account Number: ");
+		long reciver_account_number = scanner.nextLong();
+		System.out.println("Enter amount: ");
+		double amount = scanner.nextDouble();
+		scanner.nextLine();
+		System.err.println("Enter Security Pin: ");
+		String security_pin = scanner.nextLine();
+	}
 }
